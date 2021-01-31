b576f08d496dbe1449151e53262477d857d3ae4c
Alvin Khair <khairalvin@gmail.com>
2021-02-01 05:55:39 +0700

    first commit

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/tutorialedge/go-grpc-tutorial/chat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":9000")
+
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+
+	}
+
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC server over port 9000: %v", err)
+	}
+}
